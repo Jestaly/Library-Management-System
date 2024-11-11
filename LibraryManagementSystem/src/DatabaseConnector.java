f04471cc6e32a57812806ba1739a4ef47bfca3f0
Jzy <jestalycastillo15@gmail.com>
2024-11-11 12:20:04 +0800

    added db connector func

--- LibraryManagementSystem/src/DatabaseConnector.java
+++ LibraryManagementSystem/src/DatabaseConnector.java
@@ -1,23 +1,46 @@
 import java.sql.*;
 
 public class DatabaseConnector {
-    static final String DB_URL = "jdbc:mysql://localhost:3307/books";
-    static final String USER = "root";
-    static final String PASS = "joseph";
-    static final String QUERY = "SELECT * FROM book";
+    private static final String DB_URL = "jdbc:mysql://localhost:3307/books";
+    private static final String USER = "root";
+    private static final String PASS = "joseph";
+    private static final String QUERY = "SELECT * FROM book";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // Open a connection
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                PreparedStatement preparedStatement = conn.prepareStatement(QUERY);) {
-            // Extract data from result set
-            preparedStatement.setInt(1, 3);
-            preparedStatement.setString(2, "GONE");
-            preparedStatement.setString(3, "Michael Grant");
-            preparedStatement.setDate(4, java.sql.Date.valueOf("2019-10-10"));
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+        Statement statement = connect().createStatement();
+        ResultSet resultSet = statement.executeQuery(QUERY);
+        int columnCount = resultSet.getMetaData().getColumnCount();
+
+        for (int i = 1; i <= columnCount; i++) {
+            System.out.print(resultSet.getMetaData().getColumnName(i) + "\t\t");
         }
+        System.out.println();
+
+        while (resultSet.next()) {
+
+            for (int i = 1; i <= columnCount; i++) {
+                // System.out.print(resultSet.getInt(i));
+                System.out.print(resultSet.getString(i) + "\t\t");
+                // System.out.print(resultSet.getString(i));
+                // System.out.print(resultSet.getDate(i));
+            }
+            System.out.println();
+        }
+
+        // Extract data from result set
+
+        // preparedStatement.setInt(1, 3);
+        // preparedStatement.setString(2, "GONE");
+        // preparedStatement.setString(3, "Michael Grant");
+        // preparedStatement.setDate(4, java.sql.Date.valueOf("2019-10-10"));
+        // preparedStatement.executeUpdate();
+
+    }
+
+    public static Connection connect() throws SQLException {
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        return conn;
     }
 }
